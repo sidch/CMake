b9e5d045260102af7943d29f7ddd535a35a32d44
sidch <sidch@thea>
2019-04-25 13:40:08 +0530

    More tweaks to the Eigen3 script

--- Modules/FindEigen3.cmake
+++ Modules/FindEigen3.cmake
@@ -2,38 +2,41 @@
 #
 # Defines:
 #
-#   Eigen3_FOUND         True if Eigen3 was found, else false
-#   Eigen3_INCLUDE_DIRS  The directories containing the header files (Eigen is header-only)
+#   EIGEN3_FOUND        True if Eigen3 was found, else false
+#   EIGEN3_INCLUDE_DIR  The directories containing the header files (Eigen is header-only).
 #
-# To specify an additional directory to search, set Eigen3_ROOT.
+# Note that the naming (*_DIR and not *_DIRS) and capitalization is for compatibility with the FindEigen3.cmake shipped with
+# recent versions of CMake.
+#
+# To specify an additional directory to search, set EIGEN3_ROOT.
 #
 # Author: Siddhartha Chaudhuri, 2016
 #
 
-SET(Eigen3_FOUND FALSE)
+SET(EIGEN3_FOUND FALSE)
 
 # Look for the Eigen header, first in the user-specified location and then in the system locations
 SET(Eigen3_INCLUDE_DOC "The directory containing the Eigen include file Eigen/Core")
-FIND_PATH(Eigen3_INCLUDE_DIRS NAMES Eigen/Core eigen/Core Eigen/core eigen/core
-          PATHS ${Eigen3_ROOT}
+FIND_PATH(EIGEN3_INCLUDE_DIR NAMES Eigen/Core eigen/Core Eigen/core eigen/core
+          PATHS ${EIGEN3_ROOT}
           PATH_SUFFIXES eigen3 include include/eigen3
           DOC ${Eigen3_INCLUDE_DOC} NO_DEFAULT_PATH)
-IF(NOT Eigen3_INCLUDE_DIRS)  # now look in system locations
-  FIND_PATH(Eigen3_INCLUDE_DIRS NAMES Eigen/Core eigen/Core Eigen/core eigen/core
+IF(NOT EIGEN3_INCLUDE_DIR)  # now look in system locations
+  FIND_PATH(EIGEN3_INCLUDE_DIR NAMES Eigen/Core eigen/Core Eigen/core eigen/core
             PATH_SUFFIXES eigen3 include include/eigen3
             DOC ${Eigen3_INCLUDE_DOC})
-ENDIF(NOT Eigen3_INCLUDE_DIRS)
+ENDIF(NOT EIGEN3_INCLUDE_DIR)
 
-IF(Eigen3_INCLUDE_DIRS)
-  SET(Eigen3_FOUND TRUE)
-ENDIF(Eigen3_INCLUDE_DIRS)
+IF(EIGEN3_INCLUDE_DIR)
+  SET(EIGEN3_FOUND TRUE)
+ENDIF(EIGEN3_INCLUDE_DIR)
 
-IF(Eigen3_FOUND)
+IF(EIGEN3_FOUND)
   IF(NOT Eigen3_FIND_QUIETLY)
-    MESSAGE(STATUS "Found Eigen3: headers at ${Eigen3_INCLUDE_DIRS}")
+    MESSAGE(STATUS "Found Eigen3: headers at ${EIGEN3_INCLUDE_DIR}")
   ENDIF(NOT Eigen3_FIND_QUIETLY)
-ELSE(Eigen3_FOUND)
+ELSE(EIGEN3_FOUND)
   IF(Eigen3_FIND_REQUIRED)
     MESSAGE(FATAL_ERROR "Eigen3 not found")
   ENDIF(Eigen3_FIND_REQUIRED)
-ENDIF(Eigen3_FOUND)
+ENDIF(EIGEN3_FOUND)
